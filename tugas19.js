1571c2bb2257ba280c570ccd7e64f01431d366d9
Rauf <zen.star12@yahoo.com>
2021-02-14 02:22:22 +0700

    Tugas 19 Javascript

---
+++ tugas19.js
@@ -0,0 +1,17 @@
+function tugas19(){
+    var x = "Semangat...";
+    console.log(x.repeat(5))
+
+    var z = "*****";
+    console.log(z.repeat(1))
+    console.log(z.repeat(4))
+    console.log(z.repeat(6))
+    console.log(z.repeat(7))
+    console.log(z.repeat(10))
+    console.log(z.repeat(7))
+    console.log(z.repeat(6))
+    console.log(z.repeat(4))
+    console.log(z.repeat(1))
+}
+
+tugas19()
